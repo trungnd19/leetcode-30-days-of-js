e3ead976b2b141277acbd4bbe6367ae9b08b87b1
Trung Nguyen <85091105+trungnd19@users.noreply.github.com>
2023-08-21 14:23:58 +0700

    Update join-two-arrays-by-id.ts

--- join-two-arrays-by-id.ts
+++ join-two-arrays-by-id.ts
@@ -27,5 +27,36 @@ function join(arr1: any[], arr2: any[]): any[] {
 # Time complexity: The time complexity is O(nlogn) due to the sort function, where nnn is the total number of elements in the combined array (length of arr1 plus length of arr2). The iteration and merging process also contributes to the time complexity, but it is dominated by the sorting operation.
 # Space complexity: The space complexity is O(n), where nnn is the total number of elements in the map.
 
+// Example
+// Example 1:
+// Input:
+// arr1 = [
+//     {"id": 1, "x": 2, "y": 3},
+//     {"id": 2, "x": 3, "y": 6}
+// ],
+// arr2 = [
+//     {"id": 2, "x": 10, "y": 20},
+//     {"id": 3, "x": 0, "y": 0}
+// ]
+// Output:
+// [
+//     {"id": 1, "x": 2, "y": 3},
+//     {"id": 2, "x": 10, "y": 20},
+//     {"id": 3, "x": 0, "y": 0}
+// ]
+// Explanation: The two objects with id=1 and id=3 are included in the result array without modifiction. The two objects with id=2 are merged together. The keys from arr2 override the values in arr1.
+
+// Example 2:
+// Input:
+// arr1 = [
+//     {"id": 1, "b": {"b": 94},"v": [4, 3], "y": 48}
+// ]
+// arr2 = [
+//     {"id": 1, "b": {"c": 84}, "v": [1, 3]}
+// ]
+// Output: [
+//     {"id": 1, "b": {"c": 84}, "v": [1, 3], "y": 48}
+// ]
+
 // Read more
 # https://leetcode.com/problems/join-two-arrays-by-id/editorial/?envType=study-plan-v2&envId=30-days-of-javascript
