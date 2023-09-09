add2288b9fe97e1c40aa9e081d077c06a940df36
Trung Nguyen <85091105+trungnd19@users.noreply.github.com>
2023-09-09 10:21:19 +0700

    Create execute-asynchronous-functions-in-parallel.ts

---
+++ execute-asynchronous-functions-in-parallel.ts
@@ -0,0 +1,30 @@
+/**
+ * @param {Array<Function>} functions
+ * @return {Promise<any>}
+ */
+var promiseAll = async function(functions) {
+    return new Promise((resolve, reject) => {
+        // We know the resulting array will be the same length as functions
+        const results = new Array(functions.length);
+        let count = 0;
+        functions.forEach((fn, i) => {
+            fn()
+            .then(val => {
+                results[i] = val;
+                count++;
+                if(count === functions.length) resolve(results);
+            })
+            .catch(reason => reject(reason));
+        });
+    });
+};
+
+/**
+ * const promise = promiseAll([() => new Promise(res => res(42))])
+ * promise.then(console.log); // [42]
+ */
+
+
+
+// Read more
+// https://leetcode.com/problems/execute-asynchronous-functions-in-parallel/solutions/3888598/promises-are-confusing-i-wrote-some-notes-down-for-you/?envType=study-plan-v2&envId=30-days-of-javascript
